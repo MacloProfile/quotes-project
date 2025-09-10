f5cd3d17083b41c483fd2f83cb56994343925ddb
Maclo <mr.lobzuk@gmail.com>
2025-09-10 16:20:39 +0300

    add postgresql and model for quotes

--- app/views.py
+++ app/views.py
@@ -4,4 +4,20 @@ import random
 
 
 def index(request):
-    return render(request, 'index.html')
+    quotes = list(Quote.objects.all())
+    quote = None
+
+    if quotes:
+        weights = [q.weight for q in quotes]
+
+        quote = random.choices(quotes, weights=weights, k=1)[0]
+
+        quote.views += 1
+        quote.save()
+
+    return render(request, 'index.html', {'quote': quote})
+
+
+def top_quotes(request):
+    top = Quote.objects.order_by('-likes')[:10]
+    return render(request, 'top.html', {'top_quotes': top})
--- quotes_project/settings.py
+++ quotes_project/settings.py
@@ -56,3 +56,15 @@ LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
