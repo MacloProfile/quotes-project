3d44a97594288c8e7f6a2a88dfe6e8e55e70e9cb
Maclo <mr.lobzuk@gmail.com>
2025-09-12 20:13:12 +0300

    add restrictions on length of quote

--- app/forms/quote_add_form.py
+++ app/forms/quote_add_form.py
@@ -7,3 +7,13 @@ class QuoteAdd(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ['quote', 'source', 'weight']
+
+    def clean(self):
+        quote = self.cleaned_data['quote']
+
+        if len(quote) > 300:
+            raise forms.ValidationError("Слишком длинная цитата (максимум 300 символов)")
+
+        if len(quote) < 3:
+            raise forms.ValidationError("Слишком короткая цитата (минимум 3 символов)")
+
