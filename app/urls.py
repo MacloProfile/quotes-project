cf23fe6d33fe6ab2e8060451bd5ea1edb16a9552
Maclo <mr.lobzuk@gmail.com>
2025-10-15 04:09:07 +0300

    add password recovery form

--- app/urls.py
+++ app/urls.py
@@ -27,5 +27,32 @@ urlpatterns = [
 
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='password_reset/reset_form.html',
+             email_template_name='password_reset/reset_email.html',
+             success_url='/password_reset/done/'
+         ),
+         name='password_reset'),
+
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='password_reset/reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='password_reset/reset_confirm.html',
+             success_url='/reset/done/'
+         ),
+         name='password_reset_confirm'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='password_reset/reset_complete.html'
+         ),
+         name='password_reset_complete')
 ]
